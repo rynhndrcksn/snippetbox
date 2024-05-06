19aa8bf36d4e6b6c5bfde3bd235d7dfb75c7144e
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-06 14:41:04 -0700

    Add routes.go
    Refactor main.go to pull the routes out and put them in routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,37 +13,19 @@ type application struct {
 }
 
 func main() {
-	// setup flags and parse them
 	addr := flag.String("addr", ":8080", "HTTP port")
 	staticDir := flag.String("staticDir", "./ui/static", "Static assets directory")
 	flag.Parse()
 
-	// setup slog
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	app := &application{
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	// create a file server which serves files out of the "./ui/static" directory
-	// path given is relative to the project directory root
-	fileServer := http.FileServer(http.Dir(*staticDir))
-
-	// use mux.Handle() to register the file server as a handler for all URL paths that start with "/static/"
-	// for match paths, we strip the "/static" prefix before the request reaches the file server
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// register application routes
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes(*staticDir))
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
---
+++ cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func (app *application) routes(staticDir string) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// create a file server which serves files out of the "./ui/static" directory
+	// path given is relative to the project directory root
+	fileServer := http.FileServer(http.Dir(staticDir))
+
+	// use mux.Handle() to register the file server as a handler for all URL paths that start with "/static/"
+	// for match paths, we strip the "/static" prefix before the request reaches the file server
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// register application routes
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
+	return mux
+}
