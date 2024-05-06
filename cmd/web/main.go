25a6acc21e768b12c3545484fd85a6c345620aa9
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-06 11:26:59 -0700

    Complete chatper 02.09
    Update files to use static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,16 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// create a file server which serves files out of the "./ui/static" directory
+	// path given is relative to the project directory root
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// use mux.Handle() to register the file server as a handler for all URL paths that start with "/static/"
+	// for match paths, we strip the "/static" prefix before the request reaches the file server
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// register application routes
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
