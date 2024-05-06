987050c2ce158acfdf1c4b202fec6f73b4c1cccc
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-06 08:21:36 -0700

    Update main.go
    Now shows a pretty basic web application.

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
 
 func main() {
-  fmt.Println("Hello, world!")
+	// make a new server mux and register "/" to the home function
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// print a log message saying the server started
+	log.Print("Starting server on :8080")
+
+	// start our server and log any fatal errors that get returned
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
