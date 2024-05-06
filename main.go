0123d5adf52401fd2b5a556fb54a6a52c162d262
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-06 08:31:44 -0700

    Finish 02.03

--- main.go
+++ main.go
@@ -9,10 +9,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Displays a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
+}
+
 func main() {
 	// make a new server mux and register "/" to the home function
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) // {$} is a special character sequence that restricts "/" to only "/" instead of being a wildcard
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// print a log message saying the server started
 	log.Print("Starting server on :8080")
