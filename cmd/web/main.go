4a6fdc7078700d1fa5fd935fb74ecd7f248c1f81
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-06 13:30:06 -0700

    Add CLI options for executing webapp

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// setup flags and parse them
+	addr := flag.String("addr", ":8080", "HTTP port")
+	staticDir := flag.String("staticDir", "./ui/static", "Static assets directory")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	// create a file server which serves files out of the "./ui/static" directory
 	// path given is relative to the project directory root
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(*staticDir))
 
 	// use mux.Handle() to register the file server as a handler for all URL paths that start with "/static/"
 	// for match paths, we strip the "/static" prefix before the request reaches the file server
@@ -22,8 +28,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :8080")
+	log.Printf("starting server on %s", *addr)
 
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
