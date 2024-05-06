dccf245fb3c5e431184fb955fb185b34627b14ee
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-06 10:14:20 -0700

    Fix port typo

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,8 +12,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	log.Print("starting server on :8080")
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
