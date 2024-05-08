9e8be630ffa290f9ab98d3e88c2ad7ef3292016a
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-07 18:03:30 -0700

    Add db connection to main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // application struct holds the application-wide dependencies
@@ -15,9 +18,22 @@ type application struct {
 func main() {
 	addr := flag.String("addr", ":8080", "HTTP port")
 	staticDir := flag.String("staticDir", "./ui/static", "Static assets directory")
+	dbConn := flag.String("dbConn", "web:Password1!@(localhost)/snippetbox?parseTime=true", "Database connection string")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(*dbConn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer func(db *sql.DB) {
+		err = db.Close()
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
+	}(db)
 
 	app := &application{
 		logger: logger,
@@ -25,9 +41,28 @@ func main() {
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes(*staticDir))
+	err = http.ListenAndServe(*addr, app.routes(*staticDir))
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		err = db.Close()
+		if err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	return db, nil
 }
