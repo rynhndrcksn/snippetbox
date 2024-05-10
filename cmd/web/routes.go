77fac6681023598d5286ebee0f91d80716668bef
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-09 17:00:06 -0700

    Add alice to chain middleware
    Update some comments so they're more consistent.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,23 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routes(staticDir string) http.Handler {
 	mux := http.NewServeMux()
 
-	// create a file server which serves files out of the "./ui/static" directory
-	// path given is relative to the project directory root
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Path given is relative to the project directory root.
 	fileServer := http.FileServer(http.Dir(staticDir))
 
-	// use mux.Handle() to register the file server as a handler for all URL paths that start with "/static/"
-	// for match paths, we strip the "/static" prefix before the request reaches the file server
+	// Use mux.Handle() to register the file server as a handler for all URL paths that start with "/static/"
+	// For matching paths, we strip the "/static" prefix before the request reaches the file server.
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	// register application routes
+	// Register application routes.
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	// Create a middleware chain containing our "standard" middleware
+	// that should be used on every request the webapp receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
+
+	// If not using alice, middleware must be chained like this.
+	//return app.recoverPanic(app.logRequest(commonHeaders(mux)))
 }
